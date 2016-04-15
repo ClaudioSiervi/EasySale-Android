8745c70716b93bc1f57b2c974dee333f804f7d61
ClaudioSiervi <claudio.siervi@gmail.com>
2016-04-15 00:18:29 -0300

    Métodos para inserção produtos no banco (realm) Métodos para chamar os buttons da mainActivity

--- app/src/main/java/com/siervi/claudio/easesale/MainActivity.java
+++ app/src/main/java/com/siervi/claudio/easesale/MainActivity.java
@@ -4,32 +4,74 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    // main activity objects
+    Button btnProductRegistration, btnMakeSale, btnReport, btnConfig;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setUI();
+        setActions();
     }
 
-
-    public void call_activity_sale(View view) {
-        Intent intent = new Intent(this, SalesActivity.class);
-        startActivity(intent);
+    // address buttons id
+    private void setUI(){
+        btnProductRegistration = (Button) findViewById(R.id.btn_ProductRegistration );
+        btnMakeSale = (Button) findViewById(R.id.btn_MakeSale );
+        btnReport = (Button) findViewById(R.id.btn_Report );
+        btnConfig = (Button) findViewById(R.id.btn_Config );
     }
 
-    public void call_activity_product_registration(View view) {
+    // set buttons methods
+    private void setActions(){
+        btnProductRegistration.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                productRegistrationActivity(v);
+            }
+        });
+
+        btnMakeSale.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                saleActivity(v);
+            }
+        });
+
+        btnReport.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                reportActivity(v);
+            }
+        });
+
+        btnConfig.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                configActivity(v);
+            }
+        });
+
+    }
+
+    // methods
+    public void productRegistrationActivity(View view) {
         Intent intent = new Intent(this, ProductRegistrationActivity.class);
         startActivity(intent);
     }
 
-    public void call_activity_report(View view) {
+    public void saleActivity(View view) {
+        Intent intent = new Intent(this, SalesActivity.class);
+        startActivity(intent);
+    }
+
+    public void reportActivity(View view) {
         Intent intent = new Intent(this, ReportActivity.class);
         startActivity(intent);
     }
 
-    public void call_activity_config(View view) {
+    public void configActivity(View view) {
         Intent intent = new Intent(this, ConfigActivity.class);
         startActivity(intent);
     }
--- app/src/main/java/com/siervi/claudio/easesale/ProductRegistrationActivity.java
+++ app/src/main/java/com/siervi/claudio/easesale/ProductRegistrationActivity.java
@@ -3,12 +3,73 @@ package com.siervi.claudio.easesale;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import io.realm.Realm;
 
 public class ProductRegistrationActivity extends AppCompatActivity {
+
+    Button btnRegisterProduct;
+    EditText edtProductName, edtProductPrice;
+    private Realm realm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_registration);
-        Intent intent = getIntent();
+        setUI();
+        setActions();
+
+        realm = Realm.getDefaultInstance();
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        realm.close();
+    }
+
+    private void setUI() {
+        btnRegisterProduct = (Button) findViewById(R.id.btn_RegisterProduct);
+        edtProductName = (EditText) findViewById(R.id.edt_ProductName);
+        edtProductPrice = (EditText) findViewById(R.id.edt_ProductPrice);
+    }
+
+    private void setActions() {
+        btnRegisterProduct.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                registerProduct(v);
+            }
+        });
+    }
+
+    // IMPLEMENTAR O MÉTODO DE INSERÇÃO
+    private  void registerProduct(View view){
+
+    try{
+    // Salva o novo ingresso no banco de dados
+    realm.beginTransaction();
+
+    ProductList product = realm.createObject(ProductList.class);
+
+    product.setName(edtProductName.getText().toString());
+        product.setPrice(Double.parseDouble(edtProductPrice.getText().toString()));
+
+    realm.commitTransaction();
+
+
+
+    } catch (Exception e){
+        Log.e("Realm Error", "error", e);
+    }
+
+
+// clean view
+    edtProductName.setText("");
+    edtProductPrice.setText("");
+    }
+
 }
